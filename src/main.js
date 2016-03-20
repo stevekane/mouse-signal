f80ed4a45d715b2dcb120a9223359cc154b0eb30
Steve Kane <kanesteven@gmail.com>
2016-03-20 14:56:22 -0500

    essential state machine for local mouse space seems to be working

--- src/main.js
+++ src/main.js
@@ -1,32 +1,73 @@
+var MOUSE_MODE = {
+  INACTIVE: 0,
+  ACTIVE: 1,
+  JUST_DOWN: 2,
+  DOWN: 3,
+  JUST_UP: 4
+}
+
+function MouseState (el) {
+  this.el = el
+  this.mode = MOUSE_MODE.INACTIVE
+  this.previousMode = MOUSE_MODE.INACTIVE
+  this.downDuration = 0
+  this.down = [ 0, 0 ]
+  this.up = [ 0, 0 ]
+  this.previous = [ 0, 0 ]
+  this.current = [ 0, 0 ]
+}
+
+function updateMouser (dT, mouser) {
+  if      (mouser.previousMode === MOUSE_MODE.JUST_DOWN) mouser.mode = MOUSE_MODE.DOWN 
+  else if (mouser.previousMode === MOUSE_MODE.JUST_UP)   mouser.mode = MOUSE_MODE.ACTIVE
+  else {}
+  mouser.downDuration = mouser.mode === MOUSE_MODE.DOWN ? mouser.downDuration + dT : 0
+  mouser.previous[0] = mouser.current[0]
+  mouser.previous[1] = mouser.current[1]
+  mouser.previousMode = mouser.mode
+}
+
 function Mouser (body) {
   var self = this
 
   function mousemove (e) {
-    self.previous[0] = self.current[0]
-    self.previous[1] = self.previous[1]
+    if (self.mode === MOUSE_MODE.INACTIVE) self.mode = MOUSE_MODE.ACTIVE
     self.current[0] = e.clientX
     self.current[1] = e.clientY
   }
 
+  function mouseenter (e) {
+    self.mode = MOUSE_MODE.ACTIVE
+  }
+
+  function mouseleave (e) {
+    self.mode = MOUSE_MODE.INACTIVE
+  }
+
+  function blur () {
+    self.mode = MOUSE_MODE.INACTIVE
+  }
+
   function mousedown (e) {
+    e.preventDefault()
+    if (self.mode !== MOUSE_MODE.JUST_DOWN) self.mode = MOUSE_MODE.JUST_DOWN
     self.down[0] = e.clientX
     self.down[1] = e.clientY
-    mousemove(e)
   }
 
   function mouseup (e) {
+    self.mode = MOUSE_MODE.JUST_UP
     self.up[0] = e.clientX
     self.up[1] = e.clientY
-    mousemove(e)
   }
 
   body.addEventListener('mousemove', mousemove)
   body.addEventListener('mousedown', mousedown)
   body.addEventListener('mouseup', mouseup)
-  self.down = [ 0, 0 ]
-  self.up = [ 0, 0 ]
-  self.previous = [ 0, 0 ]
-  self.current = [ 0, 0 ]
+  body.addEventListener('mouseenter', mouseenter)
+  body.addEventListener('mouseleave', mouseleave)
+  body.addEventListener('blur', blur)
+  MouseState.call(this, body)
 }
 
 function randInt (min, max) {
@@ -42,25 +83,47 @@ function randColor (minR, maxR, minG, maxG, minB, maxB, alpha) {
          ')'
 }
 
-function Local (el, mouser) {
-  var bcr = el.getBoundingClientRect()
-  var collides = bcr.left < mouser.current[0] && 
-                 bcr.right > mouser.current[0] &&
-                 bcr.top < mouser.current[1] &&
-                 bcr.bottom > mouser.current[1]
+function collidesBcrPoint (bcr, pt) {
+  return bcr.left < pt[0] && 
+         bcr.right > pt[0] &&
+         bcr.top < pt[1] &&
+         bcr.bottom > pt[1]
+}
 
-  this.up = collides 
-    ? [ mouser.up[0] - bcr.left, mouser.up[1] - bcr.top ]
-    : null
-  this.down = collides 
-    ? [ mouser.down[0] - bcr.left, mouser.down[1] - bcr.top ]
-    : null
-  this.previous = collides 
-    ? [ mouser.previous[0] - bcr.left, mouser.previous[1] - bcr.top ]
-    : null
-  this.current = collides 
-    ? [ mouser.current[0] - bcr.left, mouser.current[1] - bcr.top ]
-    : null
+function getLocalX (bcr, pt) {
+  return Math.round(pt[0] - bcr.left)
+}
+
+function getLocalY (bcr, pt) {
+  return Math.round(pt[1] - bcr.top)
+}
+
+function updateMouseState (dT, mouser, ms) {
+  var bcr = local.el.getBoundingClientRect()  
+  var collides = collidesBcrPoint(bcr, mouser.current)
+
+  if (!collides) {
+    ms.mode = MOUSE_MODE.INACTIVE  
+    ms.downDuration = 0
+  }
+  else {
+    if      (ms.previousMode === MOUSE_MODE.INACTIVE)                                   ms.mode = MOUSE_MODE.ACTIVE
+    else if (mouser.mode === MOUSE_MODE.JUST_DOWN)                                      ms.mode = MOUSE_MODE.JUST_DOWN
+    else if (mouser.mode === MOUSE_MODE.JUST_UP && ms.previousMode === MOUSE_MODE.DOWN) ms.mode = MOUSE_MODE.JUST_UP
+    else if (ms.previousMode === MOUSE_MODE.JUST_DOWN)                                  ms.mode = MOUSE_MODE.DOWN
+    else if (ms.previousMode === MOUSE_MODE.JUST_UP)                                    ms.mode = MOUSE_MODE.ACTIVE
+    else                                                                                ms.mode = ms.mode
+    ms.downDuration = ms.previousMode === MOUSE_MODE.DOWN ? ms.downDuration + dT : 0
+    ms.up[0] = getLocalX(bcr, mouser.up) 
+    ms.up[1] = getLocalY(bcr, mouser.up)
+    ms.down[0] = getLocalX(bcr, mouser.down)
+    ms.down[1] = getLocalY(bcr, mouser.down)
+    ms.previous[0] = getLocalX(bcr, mouser.previous)
+    ms.previous[1] = getLocalY(bcr, mouser.previous)
+    ms.current[0] = getLocalX(bcr, mouser.current)
+    ms.current[1] = getLocalY(bcr, mouser.current)
+  }
+  ms.previousMode = ms.mode
 }
 
 var TARGETS = document.getElementsByClassName('target')
@@ -68,25 +131,45 @@ var GLOBAL = document.getElementById('global')
 var LOCAL = document.getElementById('local')
 var FOCUSED = TARGETS[randInt(0, TARGETS.length)]
 var mouser = new Mouser(document.body)
-var local = new Local(FOCUSED, mouser)
+var local = new MouseState(FOCUSED)
 var state = {
   mouser: mouser,
   local: local
 }
 
-// Setup
 for (var i = 0, T; T = TARGETS[i++];) {
   T.style.backgroundColor = randColor(150, 256, 0, 0, 0, 0, 1)
 }
 
 FOCUSED.style.backgroundColor = randColor(0, 0, 0, 0, 150, 256, 1)
 
-window.state = state
-// Main loop
-requestAnimationFrame(function update () {
-  requestAnimationFrame(update)
-  state.local = new Local(FOCUSED, state.mouser)
+var last = Date.now()
+var current = Date.now()
+var dT = current - last
 
-  GLOBAL.innerText = state.mouser.current
-  LOCAL.innerText = state.local.current
+function doSomething () {
+  if (state.local.mode === MOUSE_MODE.JUST_UP) {
+    if (state.local.downDuration > 2000) {
+      console.log('You held that thing like a boss') 
+    }
+    else {
+      console.log('held for ' + state.local.downDuration)  
+    }
+  }
+}
+
+requestAnimationFrame(function update () {
+  last = current
+  current = Date.now()
+  dT = current - last
+
+  updateMouser(dT, state.mouser)
+  updateMouseState(dT, state.mouser, state.local)
+  doSomething()
+
+  GLOBAL.innerText = JSON.stringify(state.mouser, null, 2)
+  LOCAL.innerText = JSON.stringify(state.local, null, 2)
+
+  requestAnimationFrame(update)
 })
+window.state = state
